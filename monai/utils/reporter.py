9c3f377c499b55aa646ae70e348575411d1ac8a3
Andrey <andrey@simiotics.com>
2021-03-29 21:29:24 +0300

    Add PyTorch version.

--- monai/utils/reporter.py
+++ monai/utils/reporter.py
@@ -2,7 +2,10 @@
 from humbug.consent import HumbugConsent
 from humbug.report import Reporter, Modes, Report
 
+from .module import get_torch_version_tuple
+
 import uuid
+from pprint import pprint
 from .._version import get_versions
 
 HUMBUG_TOKEN = "e10fbd54-71b0-4e68-80b0-d59ec3d99a81"
@@ -15,28 +18,32 @@ session_id_tag = "session_id:{}".format(session_id)
 version_tag = f"{version_dict.get('version', '0+unknown')}-dev-reporting"
 revision_tag = version_dict.get("full-revisionid", None)
 
-monay_version_tag = "version:{}".format(version_tag)
-monay_revision_tag = "revision:{}".format(revision_tag)
 
-monay_tags = [monay_version_tag,monay_revision_tag]
+torch_version = ".".join([str(i) for i in get_torch_version_tuple()])
+if not torch_version:
+    torch_version = 'unknown'
+
+monai_version_tag = "version:{}".format(version_tag)
+monai_revision_tag = "revision:{}".format(revision_tag)
+tourch_version_tag = "pytorch:{}".format(torch_version)
 
 
 
-def get_reporter(mode=Modes.DEFAULT):
-    monay_consent = HumbugConsent(True)
-    monay_reporter = Reporter(
-        name="MONAI",
-        consent=monay_consent,
-        client_id=None,
-        session_id=session_id,
-        bugout_token=HUMBUG_TOKEN,
-        bugout_journal_id=HUMBUG_KB_ID,
-        mode=mode
-    )
-    return monay_reporter
+monai_tags = [monai_version_tag, monai_revision_tag, tourch_version_tag]
 
+
+
+monai_consent = HumbugConsent(True)
+monai_reporter = Reporter(
+    name="MONAI",
+    consent=monai_consent,
+    client_id=None,
+    session_id=session_id,
+    bugout_token=HUMBUG_TOKEN,
+    bugout_journal_id=HUMBUG_KB_ID,
+    mode=Modes.DEFAULT
+)
 
 def setup_excepthook():
-    reporter_obj = get_reporter()
-    reporter_obj.system_report(publish=True, tags=monay_tags)
-    reporter_obj.setup_excepthook(publish=True, tags=monay_tags)
+    monai_reporter.system_report(publish=True, tags=monai_tags)
+    monai_reporter.setup_excepthook(publish=True, tags=monai_tags)
